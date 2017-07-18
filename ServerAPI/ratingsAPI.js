8185bc5590403f9bbe3b9dacdecc2c6872dc8c75
Daniel <daniel.dungngo@gmail.com>
2017-07-18 17:06:45 +0700

    update API

--- ServerAPI/ratingsAPI.js
+++ ServerAPI/ratingsAPI.js
@@ -13,7 +13,11 @@ const ratingAPI = () => ({
   rating: `${Math.floor(Math.random() * 9)}.${Math.floor(Math.random() * 9)}`,
 });
 const ratedShows = showsObj.shows.map(show => Object.assign(ratingAPI(), show));
-
+app.get('/', (rq, rs) =>
+  rs.send(
+    'This is a node JS server for api fetching. Nothing too interesting here. If you are interest go to fb.com/DanielDNgo'
+  )
+);
 app.get('/:id', (req, res) => {
   const show = ratedShows.find(item => item.imdbID === req.params.id);
   if (show) {
